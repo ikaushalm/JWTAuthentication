e9729c7e33417022a935f6005fa648caf3acd123
ikaushalm <trickykaushal@gmail.com>
2024-10-20 00:33:37 +0530

    Added Signup

--- JWTAuthentication/Models/AppUser.cs
+++ JWTAuthentication/Models/AppUser.cs
@@ -6,4 +6,15 @@ public class AppUser:IdentityUser
     [PersonalData]
     [Column(TypeName= "VARCHAR(50)")]
     public string FullName { get; set; }
+
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(10)")]
+    public string Gender { get; set; }
+
+    [PersonalData]
+    public DateOnly DOB { get; set; }
+
+    [PersonalData]
+    public int? LibraryID { get; set; }
 }
--- JWTAuthentication/Program.cs
+++ JWTAuthentication/Program.cs
@@ -1,6 +1,9 @@
+using Azure;
 using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +19,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddIdentityApiEndpoints<AppUser>()
     .AddEntityFrameworkStores<AppDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.User.RequireUniqueEmail = true;
+ 
+
+
+}
+    );
+
 builder.Services.AddDbContext<AppDbContext>(option=>option.UseSqlServer(builder.Configuration.GetConnectionString("devdb")));
 
 
@@ -26,6 +40,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseDeveloperExceptionPage<>(options=> options.);
+
 }
 
 app.UseAuthorization();
@@ -38,6 +54,36 @@ app.MapGroup("/api").
     MapIdentityApi<AppUser>();
 
 
+app.MapPost("/api/SignUp", [AllowAnonymous] async (UserManager<AppUser> usrmgr, [FromBody] RegistrationModel regmodel) =>
+{
+AppUser user = new AppUser{
+    FullName=regmodel.fullName,
+    Email=regmodel.Email,
+    UserName=regmodel.Email
+
+};
+    var response=await usrmgr.CreateAsync(user,regmodel.Password);
+
+    if (response.Succeeded)
+    {
+        return Results.Ok(response);
+    }
+    else
+    {
+        return Results.BadRequest(response);    
+    }
+
+});
+
+
 
 app.Run();
 
+public class RegistrationModel
+{
+    public string Email { get; set; }
+    public string fullName { get; set; }
+
+    public string Password { get; set; }
+
+}
